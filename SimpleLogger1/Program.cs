d6fd86d0196e0f589ae194cfae78e5139c66d4df
CBAUser <CBAUser@TRAINING4>
2023-06-27 19:15:38 -0400

    Add project files.

---
+++ SimpleLogger1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+
+namespace SimpleLoggerr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var SimpleLoggerobj = new SimpleLogger { LogFilePath = @"c:/Projects/logger.txt" };
+
+           bool success = SimpleLoggerobj.WriteToLog(DateTime.Now," " , SimpleLogger.Severity.Mid ); 
+            if(success )
+            {
+                Console.WriteLine("The info was logged succesfully");
+            }
+
+            else
+            {
+                Console.WriteLine("Log failed");
+            }
+
+         
+
+            
+
+
+        }
+    }
+}
+
---
+++ SimpleLogger1/simplelogger1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace SimpleLoggerr
+{
+    public class SimpleLogger
+    {
+        public string LogFilePath { get; set; }
+
+        public enum Severity
+        {
+            VeryLow,
+            Low,
+            Mid,
+            High,
+            ExtremyHigh
+        };
+
+        public bool WriteToLog(DateTime eventTime, string whatHappened, Severity severityLevel)
+        {
+            try
+            {
+                var logStr = $"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}, {whatHappened}, {severityLevel}";
+
+                using (StreamWriter writer = new StreamWriter(LogFilePath, true))
+                {
+                    writer.WriteLine($"({DateTime.Now.ToShortDateString()}, {DateTime.Now.ToLongTimeString()}, {whatHappened}, {severityLevel}");
+                    
+                    return true;
+
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
+
